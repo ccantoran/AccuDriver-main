48df4f91af266568e0ece811e8019b81de180eef
ccantoran <cathycantoran@live.com>
2022-11-15 19:14:10 -0600

    first commit

---
+++ controllers/drivers.js
@@ -0,0 +1,101 @@
+const cloudinary = require("../middleware/cloudinary");
+const Driver = require("../models/Driver");
+const Start = require("../models/Start");
+const Stop = require("../models/Stop");
+
+
+module.exports = {
+  getDashboard: async (req, res) => {
+    try {
+      const drivers = await Driver.find({ user: req.user.id });
+      const start = await Start.find({user: req.user.id})
+      const stop = await Stop.find({user: req.user.id})
+
+  
+      res.render("dashboard.ejs", { 
+        drivers: drivers, user: req.user, start: start, stop:stop
+      });
+
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  getDriver: async (req, res) => {
+    try {
+      const driver = await Driver.findById(req.params.id);
+      res.render("driver.ejs", { driver: driver, user: req.user });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  createDriver: async (req, res) => {
+    try {
+      // Upload image to cloudinary
+      const result = await cloudinary.uploader.upload(req.file.path);
+
+      await Driver.create({
+        image: result.secure_url,
+        cloudinaryId: result.public_id,
+        name: req.body.name,
+        user: req.user.id,
+      });
+      console.log("Driver has been added!");
+      res.redirect("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteDriver: async (req, res) => {
+    try {
+      // Find driver by id
+      let driver = await Driver.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      await cloudinary.uploader.destroy(driver.cloudinaryId);
+      // Delete driver from db
+      await Driver.remove({ _id: req.params.id });
+      console.log("Deleted Driver");
+      res.redirect("/dashboard");
+    } catch (err) {
+      res.redirect("/dashboard");
+    }
+  },
+  startTime: async (req, res) => {
+    try {
+      await Start.create({
+        user: req.user.id,
+        time: Date.now(),
+        id:req.params._id,
+      });
+      console.log("Start time has been added!");
+      res.redirect("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  stopTime: async (req, res) => {
+    try {
+      await Stop.create({
+        user: req.user.id,
+        time: Date.now(),
+        id:req.params._id,
+      });
+      console.log("Stop time has been added!");
+      res.redirect("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
+
+
+//alright... the meat and cheese?
+//drivers is i  and then we have two loops that both have conditionals 
+//we need to have another conditional.
+
+// if stop[y] = undefined{
+//   return variable = "missing stop point"
+
+// }else{
+  // do subtraction 
+// }
+// I think we have to loop thorugh the stop and start stamps 
---
+++ server.js
@@ -0,0 +1,79 @@
+const { MongoClient } = require('mongodb');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const driverRoutes = require("./routes/driver");
+const PORT = process.env.PORT || 2121
+const uri = process.env.DB_STRING;
+const client = new MongoClient(uri);
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
+
+// Passport config
+require("./config/passport")(passport);
+
+//Connect To Database
+connectDB();
+
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Static Folder
+app.use(express.static("public"));
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING}),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Use flash messages for errors, info, ect...
+app.use(flash());
+
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes);
+app.use("/driver", driverRoutes);
+
+client.connect(err => {
+  if(err){ console.error(err); return false;}
+  // connection to mongo is successful, listen for requests
+  app.listen(PORT, () => {
+      console.log("listening for requests");
+  })
+});
+
+
+
+
+//Deleted Code
+//Server Running
+// app.listen( PORT, () => {
+//   console.log("Server is running");
+// });
